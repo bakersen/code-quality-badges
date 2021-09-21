5b9abca54a704693e79b134accb7efdef4dd1b58
Baker Sentamu <bakersen@gmail.com>
2021-09-21 08:30:53 +0300

    updated calculator js

--- calculator.js
+++ calculator.js
@@ -2,12 +2,9 @@ const add = (a, b) => a + b;
 
 const substract = (a, b) => a - b;
 
-const multiply = (a, b) => {
-    return a * b;
-}
+const multiply = (a, b) => a * b;
 
-const divide = (a, b) => {
-    return a / b;
+const divide = (a, b) => a / b;
 }
 
 module.exports = substract;
